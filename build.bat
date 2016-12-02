00764a9d7f604f48fcfca3cab69f584001bef1b9
Charly Mourglia <charly.mourglia@gmail.com>
2016-12-02 18:40:38 +0100

    updated build script

--- build.bat
+++ build.bat
@@ -3,12 +3,13 @@
 if NOT EXIST build mkdir build
 pushd build
 
-del relwrab.pdb
+if EXIST relwarb.pdb del relwarb.pdb
 
 set DEBUG="True"
 
-set C_BASE_FLAGS=/WX /W4 /wd4100 /wd4189 /wd4065 /wd4101 /FC /GR- /EHa- /nologo
-set C_DEB_FLAGS=/Z7 /Od
+set C_NOWARN=/wd4100 /wd4189 /wd4065 /wd4505 /wd4101
+set C_BASE_FLAGS=/WX /W4 /FC /GR- /EHa- /nologo %C_NOWARN% /DWIN32_LEAN_AND_MEAN
+set C_DEB_FLAGS=/Z7 /Od /D_DEBUG /D_GL_ALLOW_ERROR_CHECKING
 set C_REL_FLAGS=/Ox
 
 set LD_BASE_FLAGS=user32.lib gdi32.lib opengl32.lib /nologo /opt:ref
@@ -23,7 +24,7 @@ if %DEBUG% == "" (
 )
 
 
-cl %CFLAGS% /c ..\src\win32_relwrab.cpp
-link win32_relwrab.obj %LDFLAGS% /out:relwrab.exe
+cl %CFLAGS% /c ..\src\win32_relwarb.cpp
+link win32_relwarb.obj %LDFLAGS% /out:relwarb.exe
 
 popd
