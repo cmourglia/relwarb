8527abc18b44985c91e740a3bec6912e18bd74d6
Charly Mourglia <charly.mourglia@gmail.com>
2018-03-26 23:34:44 +0200

    Moved particle stuff in its own file

---
+++ src/relwarb_particles.cpp
@@ -0,0 +1,112 @@
+#include "relwarb_particles.h"
+
+#include "relwarb.h"
+#include "relwarb_game.h"
+
+ParticleSystem* SpawnParticleSystem(GameState* gameState, z::vec2 pos)
+{
+	uint32 idx;
+	for (idx = 0; idx < MAX_PARTICLE_SYSTEMS && gameState->particleSystems[idx].alive; ++idx)
+		;
+	Assert(idx < MAX_PARTICLE_SYSTEMS);
+
+	ParticleSystem* result = &gameState->particleSystems[idx];
+
+	Log(Log_Info, "Hello @ %.3f %.3f", pos.x, pos.y);
+
+	result->pos                = pos;
+	result->systemLife         = 2;
+	result->alive              = true;
+	result->particlesPerSecond = 1000;
+	result->particleLife       = 1;
+	result->particleLifeDelta  = 0.25;
+	result->startColor         = z::Vec4(1, 1, 1, 1);
+	result->endColor           = z::Vec4(1, 1, 1, 0);
+	result->particleBitmap     = &gameState->particleBitmap;
+	result->minAngle           = (1.0 / 3.0) * z::Pi;
+	result->maxAngle           = (2.0 / 3.0) * z::Pi;
+	result->minVelocity        = 13;
+	result->maxVelocity        = 17;
+	result->gravity            = z::Vec2(0, -20);
+
+	return result;
+}
+
+void UpdateParticleSystems(GameState* state, real32 dt)
+{
+	// Update particle systems
+	for (uint32 systemIdx = 0; systemIdx < MAX_PARTICLE_SYSTEMS; ++systemIdx)
+	{
+		ParticleSystem* system = state->particleSystems + systemIdx;
+
+		if (system->alive)
+		{
+			// Spawn new particles for the current system
+			int newParticlesCount = system->particlesPerSecond * dt;
+			// Log(Log_Debug, "system %i should spawn %i particles", systemIdx, newParticlesCount);
+			for (int particleIdx = 0; particleIdx < newParticlesCount; ++particleIdx)
+			{
+				real angle = z::GenerateRandBetween(system->minAngle, system->maxAngle);
+				real vel   = z::GenerateRandBetween(system->minVelocity, system->maxVelocity);
+
+				Particle particle;
+				particle.p         = system->pos;
+				particle.dp        = z::Vec2(vel * z::Cos(angle), vel * z::Sin(angle));
+				particle.color     = system->startColor;
+				particle.life      = z::GenerateRandNormal(system->particleLife,
+                                                      system->particleLifeDelta);
+				particle.totalLife = particle.life;
+
+				system->particles.push_back(particle);
+			}
+			// Update system lifetime
+			system->systemLife -= dt;
+			if (system->systemLife <= 0.f)
+			{
+				system->alive = false;
+			}
+		}
+
+		if (!system->particles.empty())
+		{
+			// Step particles simulation for the current system
+			for (auto& particle : system->particles)
+			{
+				particle.dp += system->gravity * dt;
+				particle.p += particle.dp * dt;
+
+				real t         = (particle.totalLife - particle.life) / (particle.totalLife);
+				particle.color = Lerp(system->startColor, system->endColor, t);
+
+				// Update particle lifetime
+				particle.life -= dt;
+			}
+
+			size_t before = system->particles.size();
+
+			std::vector<size_t> toBeRemoved;
+			size_t              particleCount = system->particles.size();
+			for (size_t i = 0; i < particleCount; ++i)
+			{
+				if (system->particles[i].life <= 0.f)
+				{
+					toBeRemoved.push_back(i);
+				}
+			}
+
+			for (size_t i = 0; i < toBeRemoved.size(); ++i)
+			{
+				std::swap(system->particles[toBeRemoved[i]],
+				          system->particles[particleCount - i - 1]);
+			}
+
+			for (size_t i = 0; i < toBeRemoved.size(); ++i)
+			{
+				system->particles.pop_back();
+			}
+
+			size_t after = system->particles.size();
+			// Log(Log_Debug, "System #%i: %zu %zu", systemIdx, before, after);
+		}
+	}
+}
--- src/relwarb_world_sim.h
+++ src/relwarb_world_sim.h
@@ -1,8 +1,6 @@
 #ifndef RELWARB_WORLD_SIM_H
 #define RELWARB_WORLD_SIM_H
 
-#include <vector>
-
 #include "relwarb_math.h"
 #include "relwarb_defines.h"
 
@@ -25,50 +23,20 @@ struct Shape
 	z::vec2 offset;
 };
 
-struct Particle
+
 {
-	z::vec2 p;
-	z::vec2 dp;
+	Entity* entity1;
+	Entity* entity2;
 
-	z::vec4 color;
+	z::vec2 normal;
+	real32  distance;
 
-	real32 totalLife;
-	real32 life;
-};
-
-struct ParticleSystem
-{
-	real32 systemLife;
-	bool32 alive = false;
-
-	z::vec2 pos;
-	int     particlesPerSecond;
-	real32  particleLife;
-	real32  particleLifeDelta;
-
-	z::vec4 startColor;
-	z::vec4 endColor;
-	Bitmap* particleBitmap;
-
-	real32 minAngle;
-	real32 maxAngle;
-
-	real32 minVelocity;
-	real32 maxVelocity;
-
-	z::vec2 gravity;
-
-	// TODO(Charly): Collision related stuff
-
-	std::vector<Particle> particles;
 };
 
 // NOTE(Charly): Create a rigid body
 //               A null mass will lead to a static object
 RigidBody* CreateRigidBody(GameState* gameState, real32 mass = 0.f);
 Shape*     CreateShape(GameState* gameState, z::vec2 size, z::vec2 offset = z::Vec2(0));
-
-ParticleSystem* SpawnParticleSystem(GameState* gameState, z::vec2 pos);
 
 void AddRigidBodyToEntity(Entity* entity, RigidBody* body);
 void AddShapeToEntity(Entity* entity, Shape* shape);
